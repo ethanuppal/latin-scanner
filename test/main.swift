053db8c228d0df855c5e65df8a0f4cb209194101
euppal <113849268+ethanuppal@users.noreply.github.com>
2023-06-24 14:01:59 -0400

    Initial commit

---
+++ test/main.swift
@@ -0,0 +1,43 @@
+// scanner: main.swift: A sample program testing the functionality of the syllabifier and scanner.
+// Copyright (C) 2021 Ethan Uppal. All rights reserved.
+
+SyllabifierTests.test {
+    $0("amīcus", ["a", "mī", "cus"], [.endingConsonant])
+    $0("traxit", ["trac", "sit"], [.doubleConsonants, .endingConsonant])
+    $0("princeps", ["prin", "ceps"], [.consecConsonants, .endingConsonant])
+    $0("formārum", ["for", "mā", "rum"], [.consecConsonants, .endingConsonant])
+    $0("autoraeda", ["au", "to", "rae", "da"], [.diphthongs, .endingVowel])
+    $0("interrete", ["in", "ter", "re", "te"], [.consecConsonants, .endingVowel])
+    $0("dormiebam", ["dor", "mi", "e", "bam"], [.consecConsonants, .fauxConsonantalI, .endingConsonant])
+    $0("arbōris", ["ar", "bō", "ris"], [.consecConsonants, .endingConsonant])
+    $0("mercātōribus", ["mer", "cā", "tō", "ri", "bus"], [.consecConsonants, .endingConsonant])
+    $0("praestāvistī", ["praes", "tā", "vis", "tī"], [.consecConsonants, .diphthongs, .endingVowel])
+    $0("subiectum", ["sub", "iec", "tum"], [.consecConsonants, .consonantalI, .endingConsonant])
+    $0("quaestiō", ["quaes", "ti", "ō"], [.consecConsonants, .fauxConsonantalI, .endingVowel, .quCombs])
+    $0("quārum", ["quā", "rum"], [.endingConsonant, .quCombs])
+    $0("obiectum", ["ob", "iec", "tum"], [.consecConsonants, .consonantalI, .endingConsonant])
+    $0("frigidō", ["fri", "gi", "dō"], [.consecConsonants, .endingVowel])
+    $0("trānsportātiōnēs", ["trāns", "por", "tā", "ti", "ō", "nēs"], [.consecConsonants, .consecVowels, .fauxConsonantalI, .endingConsonant])
+    $0("ūnius", ["ū", "ni", "us"], [.consecVowels, .fauxConsonantalI, .endingConsonant])
+    $0("arma", ["ar", "ma"], [.consecConsonants, .endingVowel])
+    $0("virumque", ["vi", "rum", "que"], [.consecConsonants, .endingVowel, .quCombs])
+    $0("canō", ["ca", "nō"], [.endingVowel])
+    $0("Trōiae", ["Trō", "iae"], [.diphthongs, .consonantalI, .endingVowel])
+    $0("iō", ["iō"], [.consonantalI, .endingVowel])
+    $0("Saturnālia", ["Sa", "tur", "nā", "li", "a"], [.consecConsonants, .fauxConsonantalI, .endingVowel])
+    $0("Caecilius", ["Cae", "ci", "li", "us"], [.diphthongs, .fauxConsonantalI, .endingConsonant])
+    $0("Commentāriī", ["Com", "men", "tā", "ri", "ī"], [.consecConsonants, .fauxConsonantalI, .endingVowel])
+    $0("dē", ["dē"], [.endingVowel])
+    $0("Bellō", ["Bel", "lō"], [.consecConsonants, .endingVowel])
+    $0("Gallicō", ["Gal", "li", "cō"], [.consecConsonants, .endingVowel])
+}//() // uncomment the `()` to run syllabifier tests
+
+// Scan the first few lines of the Aeneid
+ScannerTests.dumpScan(line: "Arma virumque canō, Trōiae quī prīmus ab ōrīs")
+ScannerTests.dumpScan(line: "Ītaliam fātō profugus Lāviniaque vēnit") // will not work (yet)
+ScannerTests.dumpScan(line: "Ītaliam fātō profugus Lāvinjaque vēnit")
+ScannerTests.dumpScan(line: "lītora, multum ille et terrīs iactātus et altō,")
+ScannerTests.dumpScan(line: "vī superum, saevae memorem Iūnōnis ob īram")
+ScannerTests.dumpScan(line: "multa quoque et bellō passus dūm cōnderet urbem")
+ScannerTests.dumpScan(line: "inferretque deōs Latiōs genus unde Latīnum,")
+ScannerTests.dumpScan(line: "Albānīque patrēs atque altae moenia Rōmae")
---
+++ test/tests.swift
@@ -0,0 +1,113 @@
+// scanner: tests.swift: Testing functions for the syllabifier and scanner.
+// Copyright (C) 2021 Ethan Uppal. All rights reserved.
+
+public struct SyllabifierTests {
+    private init() {}
+
+    public enum TestType: Int, CustomStringConvertible {
+        case doubleConsonants
+        case consecConsonants
+        case consecVowels
+        case consonantalI
+        case fauxConsonantalI
+        case endingConsonant
+        case endingVowel
+        case quCombs
+        case diphthongs
+        case fauxDiphthongs
+
+        public var description: String {
+            switch self {
+            case .doubleConsonants:
+                return "double consonants"
+            case .consecConsonants:
+                return "consecutive consonants"
+            case .consecVowels:
+                return "consecutive vowels"
+            case .consonantalI:
+                return "consonantal I"
+            case .fauxConsonantalI:
+                return "faux consonantal I"
+            case .endingConsonant:
+                return "ending consonant"
+            case .endingVowel:
+                return "ending vowel"
+            case .diphthongs:
+                return "diphthongs"
+            case .fauxDiphthongs:
+                return "faux diphthongs"
+            case .quCombs:
+                return "'qu' combinations"
+            default:
+                return ""
+            }
+        }
+
+        case end
+    }
+
+    public static func test(_ tester: @escaping ((String, [String], [TestType]) -> ()) -> ()) -> () -> () {
+        return {
+            print("➡️ Testing Syllabifier...")
+            var correct = 0
+            var total = 0
+            var problems = [Int](repeating: 0, count: TestType.end.rawValue)
+
+            func testFunc(_ word: String, _ target: [String], _ testType: [TestType]) {
+                total += 1
+                let result = Syllabifier.syllabify(word: word)
+                if result.map { $0.value } == target {
+                    print("  ✅ Correctly syllabified '\(word)'")
+                    correct += 1
+                } else {
+                    print("  ❌ Failed to syllabify '\(word)'")
+                    testType.forEach {
+                        problems[$0.rawValue] += 1
+                    }
+                }
+                print("     Produced: \(result.map { "\($0.0)(\($0.1 == nil ? "?" : ($0.1! ? "-" : "u")))" }.joined(separator: " "))")
+
+            }
+
+            tester(testFunc)
+
+            print("⬅️ Passed with a score of \(Int(Double(correct) / Double(total) * 100))% (\(correct)/\(total))")
+            if problems.contains { $0 > 0 } {
+                print("   Problems:")
+                for (index, problem) in problems.enumerated().sorted(by: { $0.1 > $1.1 }) where problem > 0 {
+                    print("   - \(Int(Double(problem) / Double(total - correct) * 100))% of failures included \(TestType(rawValue: index)!)")
+                }
+            }
+        }
+    }
+
+}
+
+public struct ScannerTests {
+    private init() {}
+
+    public static func dumpScan(line: String) {
+        let components = line.split(omittingEmptySubsequences: true) {
+            " ,.:;\"'()[]-_=+/\\`~".contains($0)
+        }.map { String($0) }
+        let results = Scanner.scan(line: components, elide: false)
+        print(results.map { $0.map { $0.value }.joined() }.joined(separator: " "))
+        for result in results {
+            for syllable in result {
+                switch syllable.isLong {
+                case .some(true):
+                    print("-", terminator: "")
+                case .some(false):
+                    print("u", terminator: "")
+                case nil:
+                    print("?", terminator: "")
+                }
+                print(String(repeating: " ", count: syllable.value.count - 1), terminator: "")
+            }
+            print(" ", terminator: "")
+        }
+        print()
+        print("The line is", Scanner.match(line: results, against: .hexameter) ? "✅ valid" : "❌ invalid", "hexameter")
+        print()
+    }
+}
